08d099d79ec45ef3656823bf3ef54a48e6e6d435
tokyoDan67 <danielyamagata12@gmail.com>
2023-03-13 21:29:03 -0400

    Wrote the boilerplate

---
+++ src/Preferences.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/// @title KyotoPay
+/// Version 1.1 
+
+import {DataTypes} from "./libraries/DataTypes.sol";
+import {IPreferences} from "./interfaces/IPreferences.sol";
+
+contract Preferences is IPreferences {}
